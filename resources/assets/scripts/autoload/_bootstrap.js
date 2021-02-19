b0146d05b783417b65617c44d264b9d0baca3a8a
erik <erik@resourceatlanta.com>
2021-02-19 11:27:31 -0600

    change bootstrap loader

--- resources/assets/scripts/autoload/_bootstrap.js
+++ resources/assets/scripts/autoload/_bootstrap.js
@@ -1,22 +1,2 @@
 // Import all of Bootstrap's JS
-//import 'bootstrap';
-
-/**
- Instead of loading everything, we're going to
- import a subset of Bootstrap's JS, in an attempt to keep the final
- JS file from getting too bloated with unused code
- */
-
-import 'bootstrap/js/dist/alert';
-import 'bootstrap/js/dist/button';
-//import 'bootstrap/js/dist/carousel';
-import 'bootstrap/js/dist/collapse';
-// import 'bootstrap/js/dist/dropdown';
-import 'bootstrap/js/dist/index';
-import 'bootstrap/js/dist/modal';
-//import 'bootstrap/js/dist/popover';
-//import 'bootstrap/js/dist/scrollspy';
-import 'bootstrap/js/dist/tab';
-//import 'bootstrap/js/dist/toast';
-// import 'bootstrap/js/dist/tooltip';
-import 'bootstrap/js/dist/util';
+import 'bootstrap';
