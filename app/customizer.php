3da2495d0654b5103aee7c521a7fd1e582f1e921
erik <erik@resourceatlanta.com>
2021-02-22 10:58:07 -0600

    add default customizer fields

--- app/customizer.php
+++ app/customizer.php
@@ -121,6 +121,77 @@ function resource_customize($wp_customize)
         'settings' => 'google_maps_longitude',
         'type' => 'number',
     ]);
+
+    /**
+     * Email
+     */
+    $wp_customize->add_setting('email_setting', [
+        'default' => '',
+        'sanitize_callback' => 'sanitize_text_field'
+    ]);
+
+    $wp_customize->add_control('email_control', [
+        'label' => __('Contact Email', 'sage'),
+        'section' => 'site_wide_info',
+        'settings' => 'email_setting',
+        'type' => 'text'
+    ]);
+
+    /**
+     * Phone
+     */
+    $wp_customize->add_setting('phone_setting', [
+        'default' => '',
+        'sanitize_callback' => 'sanitize_text_field'
+    ]);
+
+    $wp_customize->add_control('phone_control', [
+        'label' => __('Contact Phone Number', 'sage'),
+        'section' => 'site_wide_info',
+        'settings' => 'phone_setting',
+        'type' => 'tel'
+    ]);
+
+    /**
+     * Social Media Panel
+     */
+    $wp_customize->add_section('social_media', [
+        'capability' => 'edit_theme_options',
+        'title' => __('Social Media', 'sage'),
+        'description' => sprintf(__('Social media account links', 'sage')),
+        'priority' => 90
+    ]);
+
+    /**
+     * Facebook
+     */
+    $wp_customize->add_setting('facebook_setting', [
+        'default' => '',
+        'sanitize_callback' => 'esc_url_raw'
+    ]);
+
+    $wp_customize->add_control('facebook_control', [
+        'label' => __('Facebook account URL', 'sage'),
+        'section' => 'social_media',
+        'settings' => 'facebook_setting',
+        'type' => 'url'
+    ]);
+
+    /**
+     * Instagram
+     */
+    $wp_customize->add_setting('instagram_setting', [
+        'default' => '',
+        'sanitize_callback' => 'esc_url_raw'
+    ]);
+
+    $wp_customize->add_control('instagram_control', [
+        'label' => __('Instagram account URL', 'sage'),
+        'section' => 'social_media',
+        'settings' => 'instagram_setting',
+        'type' => 'url'
+    ]);
+
 }
 
 add_action('customize_register', __NAMESPACE__ . '\\resource_customize');
