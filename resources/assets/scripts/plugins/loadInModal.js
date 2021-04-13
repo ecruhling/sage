1dc369fc46fd2d4640d3ac3efb786ccfd1b5cf21
ecruhling <erik@resourceatlanta.com>
2021-04-13 16:16:38 -0500

    filter out #tab buttons on smoothScroll.js plugin
    add 'load-raster-in-modal' functionality to loadInModal.js

--- resources/assets/scripts/plugins/loadInModal.js
+++ resources/assets/scripts/plugins/loadInModal.js
@@ -1,30 +1,40 @@
 // jQuery plugin to load content in modal when clicking
 // on links with the 'load-in-modal' class
 
-function loadInModal() {
+function loadInModal()
+{
 
 // variables
-  const $modalBody = $('.content-modal-body');
-  const $modalDialog = $('#content-modal');
-  const $loading = $('#loading');
+    const $modalBody = $('.content-modal-body');
+    const $modalDialog = $('#content-modal');
+    const $loading = $('#loading');
 
-  $(document).on('click', 'a.load-in-modal, li.load-in-modal a.nav-link', function (e) {
-    e.preventDefault();
-    $loading.addClass('show');
-    $.ajax({
-      url: this.href,
-      async: true,
-      cache: false,
-      success: function (data) {
-        let $data = $(data).find('.main');
-        $modalDialog.one('shown.bs.modal', function () {
-        }).modal('show');
-        $modalBody.html($data).promise().done(function () {
-          $loading.removeClass('show');
-        });
-      },
+    $(document).on('click', 'a.load-in-modal, li.load-in-modal a.nav-link', function (e) {
+        e.preventDefault();
+        $loading.addClass('show');
+        if ($(this).hasClass('load-raster-image-in-modal')) {
+            const $data = '<img src="' + $(this).attr('href') + '" alt="' + $(this).data('alt') + '" class="' + $(this).data('class') + '" />'
+            $modalDialog.one('shown.bs.modal', function () {
+            }).modal('show');
+            $modalBody.html($data).promise().done(function () {
+                  $loading.removeClass('show');
+            });
+        } else {
+            $.ajax({
+                url: this.href,
+                async: true,
+                cache: false,
+                success: function (data) {
+                    let $data = $(data).find('.main');
+                    $modalDialog.one('shown.bs.modal', function () {
+                    }).modal('show');
+                    $modalBody.html($data).promise().done(function () {
+                        $loading.removeClass('show');
+                    });
+                },
+            });
+        }
     });
-  });
 
 }
 
--- resources/assets/scripts/plugins/smoothScroll.js
+++ resources/assets/scripts/plugins/smoothScroll.js
@@ -1,42 +1,36 @@
 // Smooth scrolling on internal page links jQuery plugin
 
-function smoothScroll() {
+function smoothScroll()
+{
 // Select all links with hashes
-  $('a[href*="#"]')
+    $('a[href*="#"]').not('.btn')
     // Remove links that don't actually link to anything
     .not('[href="#"]')
     .not('[href="#0"]')
-    .on('click', function(e) {
+    .on('click', function (e) {
       // On-page links only
-      if (
+        if (
         location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '')
         &&
         location.hostname === this.hostname
-      ) {
-        // Figure out the element to scroll to
-        const hash = this.hash;
-        let target = $(hash);
-        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-        if (target.length) { // make sure target exists
-          // Only prevent the default behavior if the animation is actually going to play
-          e.preventDefault();
-          $('html, body').animate({
-            scrollTop: $(target).offset().top - parseInt($('body').css('scroll-padding-top')), // use scroll-padding-top as offset for scroll
-          }, 450, function() {
-            // Callback after animation: change focus to target element
-            const $target = $(target);
-            window.history.pushState(null, null, hash); // update URL address
-            $target.focus();
-            if ($target.is(':focus')) { // Checks to see if target was focused already
-              return false;
-            } else {
-              $target.attr('tabindex', '-1'); // Adding tabindex for elements that are not focusable
-              $target.focus(); // Set focus again
+        ) {
+          // Figure out the element to scroll to
+            const hash = this.hash;
+            let target = $(hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) { // make sure target exists
+              // Only prevent the default behavior if the animation is actually going to play
+                e.preventDefault();
+                $('html, body').animate({
+                    scrollTop: $(target).offset().top - parseInt($('body').css('scroll-padding-top')), // use scroll-padding-top as offset for scroll
+                }, 450, function () {
+                  // Callback after animation: change focus to target element
+                    const $target = $(target);
+                    window.history.pushState(null, null, hash); // update URL address
+                    $target.focus();
+                });
             }
-
-          });
         }
-      }
     });
 }
 
